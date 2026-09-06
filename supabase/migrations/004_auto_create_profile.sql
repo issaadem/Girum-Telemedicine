1ab3075b7ecf1a88431e8fa7704a70eb6ef0b3d8
Adem Omer Adem <aadamomar88@gmail.com>
2026-09-06 13:14:08 +0300

    feat: add trigger to auto-create profile row on user signup

--- supabase/migrations/004_auto_create_profile.sql
+++ supabase/migrations/004_auto_create_profile.sql
@@ -1,5 +1,5 @@
 ﻿create function public.handle_new_user()
-returns trigger as \$\$
+returns trigger as $$
 begin
   insert into public.profiles (id, full_name, role)
   values (
@@ -9,7 +9,7 @@ begin
   );
   return new;
 end;
-\$\$ language plpgsql security definer;
+$$ language plpgsql security definer;
 
 create trigger on_auth_user_created
   after insert on auth.users
